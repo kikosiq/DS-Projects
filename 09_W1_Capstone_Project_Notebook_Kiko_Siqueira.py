ffac6993685bcc27fac20ee3daddc2d55cc4a41d
Kiko Siqueira <kikosiq@gmail.com>
2019-04-07 18:30:41 -0400

    Add files via upload

---
+++ 09_W1_Capstone_Project_Notebook_Kiko_Siqueira.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# #### 09_W1_Capstone_Project_Notebook_Kiko_Siqueira
+
+# ## Applied Data Science Capstone Project
+
+# This notebook is for the Coursera_IBM Applied Data Science Capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
